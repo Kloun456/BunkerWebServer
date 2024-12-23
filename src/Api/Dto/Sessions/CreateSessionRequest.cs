b6c7c6c4e5dd2d52f638bb1f05cb313674336324
Egor <78965361+Kloun456@users.noreply.github.com>
2024-12-23 20:04:59 +0300

    fix: убрать модели и мапинг сессий

--- src/Api/Dto/Sessions/CreateSessionRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BunkerWebServer.Api.Dto.Sessions;
-
-public class CreateSessionRequest
-{
-    public required string UserName { get; set; }
-    public required string Password { get; set; }
-}
--- src/Core/Models/Sessions/CreateSession.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BunkerWebServer.Core.Models.Sessions;
-
-public class CreateSession
-{
-    public required string UserName { get; set; }
-    public required string Password { get; set; }
-}
